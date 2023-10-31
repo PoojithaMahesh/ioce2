0390c886ca1f9542b7241cd401bce7fba0a8ecb7
unknown <poojithadarshan2131@gmail.com>
2023-10-31 20:58:06 +0530

    final

---
+++ src/main/java/withoutbean/Driver.java
@@ -0,0 +1,20 @@
+package withoutbean;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Driver {
+public static void main(String[] args) {
+	ApplicationContext applicationContext=new ClassPathXmlApplicationContext("bean.xml");
+	
+	Mobile mobile=applicationContext.getBean("mobile",Mobile.class);
+	System.out.println(mobile);
+	Charger charger=applicationContext.getBean("charger",Charger.class);
+	System.out.println(charger);
+	
+}
+}
---
+++ src/main/java/withoutbean/Mobile.java
@@ -0,0 +1,26 @@
+package withoutbean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mobile {
+    @Value("1")
+	private int id;
+    @Value("iphone")
+	private String name;
+    @Value("black")
+	private String color;
+    
+    @Autowired
+    private Charger charger;
+
+	@Override
+	public String toString() {
+		return "Mobile [id=" + id + ", name=" + name + ", color=" + color + ", charger=" + charger + "]";
+	}
+	
+	
+	
+}
